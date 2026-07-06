bd1bc22537f96e22f19d1655706d7353f2a66535
Selvam-9 <selvam.aug9@gmail.com>
2026-07-06 06:08:25 +0530

    Refactor API key and model retrieval in gemini.py
    Refactor API key and model name retrieval to use direct access from secrets. Update the generate_answer function to use the model name variable.

--- llm/gemini.py
+++ llm/gemini.py
@@ -1,26 +1,43 @@
 import os
 import streamlit as st
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
+
+# -----------------------------
+# Get API Key
+# -----------------------------
 
 api_key = os.getenv("GEMINI_API_KEY")
 
 if not api_key:
-    api_key = st.secrets.get("GEMINI_API_KEY")
+    api_key = st.secrets["GEMINI_API_KEY"]
+
+# -----------------------------
+# Get Model
+# -----------------------------
+
+model_name = os.getenv("GEMINI_MODEL")
+
+if not model_name:
+    model_name = st.secrets["GEMINI_MODEL"]
+
+# -----------------------------
+# Create Client
+# -----------------------------
+
+client = genai.Client(api_key=api_key)
+
+# -----------------------------
+# Generate Answer
+# -----------------------------
 
 def generate_answer(prompt):
-    """
-    Generate answer using Gemini
-    """
+
     response = client.models.generate_content(
-        model=os.getenv('GEMINI_MODEL'),
+        model=model_name,
         contents=prompt
     )
-    if not response:
-        response = client.models.generate_content(
-        model=st.secrets.get('GEMINI_MODEL'),
-        contents=prompt
-    )
-    
+
     return response.text
