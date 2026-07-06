0cc427b90a94237f4de57b3f4d1d389dfa6cc590
Selvam-9 <selvam.aug9@gmail.com>
2026-07-06 06:02:31 +0530

    secret updated

--- llm/gemini.py
+++ llm/gemini.py
@@ -1,11 +1,13 @@
-from google import genai
-from dotenv import load_dotenv
 import os
+import streamlit as st
+from dotenv import load_dotenv
 
 load_dotenv()
 
+api_key = os.getenv("GEMINI_API_KEY")
 
-client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
+if not api_key:
+    api_key = st.secrets.get("GEMINI_API_KEY")
 
 def generate_answer(prompt):
     """
